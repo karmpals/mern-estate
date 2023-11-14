a869144d2aaad77769212f29664f32d8c3f362eb
Karmpal <kpsheoran33@gmail.com>
2023-11-14 21:51:09 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB).then(()=>{
-    console.log("db connected");
-})
+mongoose.connect(process.env.MONGODB).then(() => {
+  console.log("db connected");
+});
 
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log('server is connected...')
-})
+app.listen(3000, () => {
+  console.log("server is connected...");
+});
