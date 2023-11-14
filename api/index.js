459a3379b55c5ef4f13ca98576be6e78747d2dee
Karmpal <kpsheoran33@gmail.com>
2023-11-14 22:17:43 +0530

    create api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB).then(() => {
@@ -12,3 +13,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("server is connected...");
 });
+
+app.use('/api/user', userRouter);
