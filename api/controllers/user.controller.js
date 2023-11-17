ec9a22e278fb07e43999bb18a37f8e13b1194985
Karmpal <kpsheoran33@gmail.com>
2023-11-17 18:42:14 +0530

    delete user func

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import User from "../modals/user.modal.js";
 import { errorHandler } from "../utils/error.js";
-import  bcryptjs  from "bcryptjs";
+import bcryptjs from "bcryptjs";
 
 export const test = (req, res) => {
   res.send("hello World");
@@ -27,6 +27,18 @@ export const updateUser = async (req, res, next) => {
     );
     const { password, ...rest } = updatedUser._doc;
     res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only delete your own account!"));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json("User has been deleted!");
   } catch (error) {
     next(error);
   }
